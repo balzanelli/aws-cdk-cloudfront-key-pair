ee5feef458d861382af1bee0d95975c09b93d07d
balzanelli <me@balzanel.li>
2023-04-07 13:08:16 +0100

    fix(test): use renamed cloudfront key pair construct

--- test/lib/test-stack.ts
+++ test/lib/test-stack.ts
@@ -1,12 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { TrustedGroupKeyPair } from '../../lib/trusted-group-key-pair';
+import { CloudFrontKeyPair } from '../../lib/cloudfront-key-pair';
 
 export class TestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new TrustedGroupKeyPair(this, 'TrustedGroupKeyPair', {
+    new CloudFrontKeyPair(this, 'CloudFrontKeyPair', {
       name: 'aws-cdk-cloudfront-key-pair',
       description: 'AWS CDK CloudFront Key Pair',
     });
