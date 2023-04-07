a83f7022dea99b021f6d64e96a56beafa7a3e08c
balzanelli <me@balzanel.li>
2023-04-07 11:11:04 +0100

    feat(trusted-group): cloudfront key group construct

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,4 @@
 export * from './key-pair-props';
 export * from './key-pair';
+export * from './trusted-group-key-pair-props';
+export * from './trusted-group-key-pair';
---
+++ lib/trusted-group-key-pair-props.ts
@@ -0,0 +1,4 @@
+export interface TrustedGroupKeyPairProps {
+  readonly name: string;
+  readonly description: string;
+}
